1dccc0bddb39519fc3d5969473a2ae2068b897ea
ChelseaW <c.winfrey93@yahoo.com>
2019-01-24 16:35:15 -0600

    Functions

---
+++ FunctionsCW.py
@@ -0,0 +1,80 @@
+
+def main():
+    # problem1()
+    # problem2()
+    # problem3()
+    problem4()
+    # challenge()
+
+# Create a function in your program that counts from 0 to [NUMBER]
+# The code is counting from 0 to any number
+
+
+# def problem1():
+#
+#     count = int(input("Enter a number"))
+#     count2 = ''
+#     for in range(0,0)
+
+
+
+
+# Create a function that has a loop that quits with ‘q’. If the user doesn't enter 'q', ask them to input another string.
+
+# def problem2():
+#
+#     user = ''
+#
+#     while user != "q":
+#         user2 = input("Give me something")
+#         print(user2 != "q")
+# It is false once you enter "q"
+
+
+
+# Create 4 functions called add, subtract, multiply, and divide.
+# Create them to allow a user to perform the name of the function to the two numbers and return the result.
+
+
+# Allowing any number to return any result
+# def problem3():
+#     print(multiply(6,5))
+#
+# def add(x,y):
+#     return(x+y)
+#
+# def subtract(x,y):
+#     return(x-y)
+#     # subtract(5-6)
+#
+# def multiply(x,y):
+#     return(x*y)
+#     # multiply(2*3)
+#
+# def divide(x,y):
+#     # divide(9,3)
+#     return(x/y)
+#
+#
+# def problem4():
+#     askUser = int(input("Give a number"))
+#
+#     askUser2 =int(input("Give a number"))
+#
+#     op = input("operation?")
+#
+#     if(op =='add'):
+#         print(add(askUser,askUser2))
+#     elif(op =='subtract'):
+#         print(subtract(askUser,askUser2))
+#     elif(op =='multiply'):
+#         print(multiply(askUser,askUser2))
+#     elif(op =='divide'):
+#         print(divide(askUser,askUser2))
+#
+#
+#
+#
+
+if __name__ == '__main__':
+    main()
